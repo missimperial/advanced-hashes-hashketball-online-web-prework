0a180acd3809de6d7bb0501ad907c518da527e22
Kathy B <kathybuggs@gmail.com>
2019-09-10 23:33:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -76,5 +76,6 @@ def player_stats(name)
         if attribute == :players
          value.each do |data|
            if data[:player_name] == name
+             binding.pry
              return data.except(:player_name)
 end end  end end end end 
