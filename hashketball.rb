ce9d1b55c5e7317fc08c21bd457eeaa5c3687378
Kathy B <kathybuggs@gmail.com>
2019-09-10 20:27:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,14 @@
 # Write your code here!
 
 def game_hash
-  team = {home: {team_name: "Brooklyn Nets", colors: ["black", "white"], players:[{"Alan Anderson"}]}, 
+  team = {home: {team_name: "Brooklyn Nets", colors: ["black", "white"], players:[
+    {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks 1},
+    {player_name: "Reggie Evans", number: 30, shoe: 14, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks 1}
+    {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks 1}
+    {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks 1}
+    {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks 1}
+    ]  }, 
+          
           away:{}}
 end
 
