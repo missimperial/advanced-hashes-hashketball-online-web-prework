d453896b4adaab5b827596885a84318ad3afec10
Kathy B <kathybuggs@gmail.com>
2019-09-10 22:11:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -23,14 +23,14 @@ def num_points_scored(name)
   #points = {}
   game_hash.each do |location , team|
     team.each do |attribute, value|
-        #if attribute == :players
+        if attribute == :players
          value.each do |data|
            if data[:player_name] == name
              return data[:points]
            #binding.pry
             end
           end
-        #end
+        end
     
     end
   end
