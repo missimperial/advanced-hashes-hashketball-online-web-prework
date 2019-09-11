ef09eb7ebd9d0a7dbd18c5b3e7e5f64a5bdc4742
Kathy B <kathybuggs@gmail.com>
2019-09-11 01:11:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -94,6 +94,6 @@ def big_shoe_rebounds
           shoe_size[data[:shoe]] = [data[:player_name], data[:rebounds]]
           rebound = shoe_size.max[1][1]
         end 
-        binding.pry
+        return rebound
        end
 end end end
