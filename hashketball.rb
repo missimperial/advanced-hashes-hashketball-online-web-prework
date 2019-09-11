86d0c6a1fe2563f61a4d9ec7e762116ed5ef3bd5
Kathy B <kathybuggs@gmail.com>
2019-09-11 00:27:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,11 +15,11 @@ def game_hash
     {"DeSagna Diop" => {number: 2 , shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 4 , blocks: 4 , slam_dunks: 5}},
     {"Ben Gordon" => {number: 8, shoe: 15, points: 33, rebounds: 3 , assists: 2 , steals: 1 , blocks: 1 , slam_dunks: 0 }},
     {"Kemba Walker" => {number: 33, shoe: 15, points: 6 , rebounds: 12, assists: 12, steals: 7 , blocks: 5 , slam_dunks: 12}}]}}
-          #.pry
+      
   team
 end
 
-
+=begin
 def num_points_scored(name)
   #points = {}
   game_hash.each do |location , team|
